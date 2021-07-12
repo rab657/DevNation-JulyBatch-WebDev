08efb97308843fab3d3394cb35208ccebb7a797e
Shahid Bahadur <shahidbahadur1998@gmail.com>
2021-07-12 20:09:39 +0500

    added Python assign #1

---
+++ Python/Python Assign #1/Q2.py
@@ -0,0 +1,20 @@
+print(1)
+print(2)
+print(3)
+print(4)
+print(5)
+print(6)
+print(7)
+print(8)
+print(9)
+print(10)
+print(11)
+print(12)
+print(13)
+print(14)
+print(15)
+
+num = ["1","2","3","4","5","6","7","8","9","10","11","12","13","14","15"]
+for i in num:
+  print(i)
+  
---
+++ Python/Python Assign #1/Q3.py
@@ -0,0 +1,4 @@
+num1 = int(input("Enter first No. : "))
+num2 = int(input("Enter Second No. : "))
+result = (num1)+ (num2)
+print(format(result, ".2f"))
